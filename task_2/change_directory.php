19e6353f9d296bc90658043c4ae54e9c0ea73a05
Jules Netze <julesnetze@gmail.com>
2020-12-16 15:17:49 +0100

    Refactor Path class

--- task_2/change_directory.php
+++ task_2/change_directory.php
@@ -8,25 +8,17 @@ class Path {
     public function getCurrentDirectory(): string { return $this->current_directory; }
 
     public function changeDirectory(string $directory_change) {
-        $new_directory = explode('/', $this->current_directory);
-        $last = $this->extractLastElementInPath($directory_change);
+        $new_directory = explode('/', $this->current_directory);    
 
-        array_splice($new_directory, -$this->extractNumberOfParentDirectories($directory_change));
-        if ($last != "..") array_push($new_directory, $last);
+        foreach (explode('/', $directory_change) as $element) {
+            if ($element == '..') {
+                array_pop($new_directory);  
+            } else {
+                array_push($new_directory, $element);
+            }
+        }
 
         $this->current_directory = implode('/', $new_directory);
-    }
-
-    private function extractNumberOfParentDirectories(string $directory): int {
-        $directory_as_array = explode('/', $directory);
-        $parent_directories_count = array_count_values($directory_as_array)['..'];
-        return $parent_directories_count;
-    }
-
-    private function extractLastElementInPath(string $directory) {
-        $directory_as_array = explode('/', $directory);
-        $last_element = end($directory_as_array);
-        return $last_element;
     }
 }
 
