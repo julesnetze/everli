4f401e153fcc2beb424f82d38254a3e3d08011b5
Jules Netze <julesnetze@gmail.com>
2020-12-17 13:58:42 +0100

    Complete haversine coverage functionality with tests

--- task_3/haversine_coverage.py
+++ task_3/haversine_coverage.py
@@ -1,42 +1,29 @@
 from haversine import haversine
+from operator import itemgetter
+
+sort_test = sorted
 
 def haversine_coverage(locations, shoppers):
     shoppers_list = []
-    
+
     for shopper in shoppers:
         if shopper.get('enabled') == False: continue
-        lat = shopper.get('lat')
-        lng = shopper.get('lng')
-        shopper_dict = {'shopper_id': shopper['id'], 'coverage': 0}
+
+        lat_shopper = shopper['lat']
+        lng_shopper = shopper['lng']
+
+        locations_covered = 0
 
         for location in locations:
-            if (haversine(location, (lat, lng)) < 10.0):
-                shopper_dict['coverage'] += 1
-
+            lat_location = location['lat']
+            lng_location = location['lng']
+            if (haversine((lat_location, lng_location), (lat_shopper, lng_shopper)) < 10.0):
+                locations_covered += 1
+        
+        shopper_dict = {'shopper_id': shopper['id'], 'coverage': int(locations_covered/len(locations) * 100) }
         shoppers_list.append(shopper_dict)
+    
+    return sort_test(shoppers_list, key=itemgetter('coverage'), reverse=True)
 
-    return shoppers_list
 
 
-locations = [
-    	  {'id': 1000, 'zip_code': '37069', 'lat': 45.35, 'lng': 10.84},
-    	  {'id': 1001, 'zip_code': '37121', 'lat': 45.44, 'lng': 10.99},
-    	  {'id': 1001, 'zip_code': '37129', 'lat': 45.44, 'lng': 11.00},
-          {'id': 1001, 'zip_code': '37133', 'lat': 45.43, 'lng': 11.02},
-]
-
-shoppers = [
-    {'id': 'S1', 'lat': 45.46, 'lng': 11.03, 'enabled': True},
-    {'id': 'S2', 'lat': 45.46, 'lng': 10.12, 'enabled': True},
-    {'id': 'S3', 'lat': 45.34, 'lng': 10.81, 'enabled': True},
-    {'id': 'S4', 'lat': 45.76, 'lng': 10.57, 'enabled': True},
-    {'id': 'S5', 'lat': 45.34, 'lng': 10.63, 'enabled': True},
-    {'id': 'S6', 'lat': 45.42, 'lng': 10.81, 'enabled': True},
-    {'id': 'S7', 'lat': 45.34, 'lng': 10.94, 'enabled': True},
-]
-
-sorted = [
-  {'shopper_id': 'S3', 'coverage': 72},
-  {'shopper_id': 'S1', 'coverage': 43},
-  {'shopper_id': 'S6', 'coverage': 12},
-]
--- task_3/haversine_coverage_test.py
+++ task_3/haversine_coverage_test.py
@@ -1,11 +1,36 @@
 import unittest
 from haversine_coverage import haversine_coverage
-from haversine import haversine
+
+locations = [
+    	  {'id': 1000, 'zip_code': '37069', 'lat': 45.35, 'lng': 10.84},
+    	  {'id': 1001, 'zip_code': '37121', 'lat': 45.44, 'lng': 10.99},
+    	  {'id': 1001, 'zip_code': '37129', 'lat': 45.44, 'lng': 11.00},
+          {'id': 1001, 'zip_code': '37133', 'lat': 45.43, 'lng': 11.02},
+]
+
+shoppers = [
+    {'id': 'S1', 'lat': 45.46, 'lng': 11.03, 'enabled': True},
+    {'id': 'S2', 'lat': 45.46, 'lng': 10.12, 'enabled': True},
+    {'id': 'S3', 'lat': 45.34, 'lng': 10.81, 'enabled': True},
+    {'id': 'S4', 'lat': 45.76, 'lng': 10.57, 'enabled': True},
+    {'id': 'S5', 'lat': 45.34, 'lng': 10.63, 'enabled': True},
+    {'id': 'S6', 'lat': 45.42, 'lng': 10.81, 'enabled': True},
+    {'id': 'S7', 'lat': 45.34, 'lng': 10.94, 'enabled': True},
+]
+
+sorted = [
+  {'shopper_id': 'S3', 'coverage': 72},
+  {'shopper_id': 'S1', 'coverage': 43},
+  {'shopper_id': 'S6', 'coverage': 12},
+]
 
 class HaversineCoverageTest(unittest.TestCase):
 
     def test_coverage_shopper(self):
-        location = [(45.35, 10.84)] 
+        location = [
+    	  {'id': 1000, 'zip_code': '37069', 'lat': 45.35, 'lng': 10.84}
+        ]
+
         shoppers = [
             {'id': 'S1', 'lat': 45.46, 'lng': 11.03, 'enabled': True}
         ]
@@ -18,7 +43,10 @@ class HaversineCoverageTest(unittest.TestCase):
         self.assertEqual(result , expected)
 
     def test_coverage_shopper_not_enabled(self):
-        location = [(45.35, 10.84)] 
+        location = [
+    	  {'id': 1000, 'zip_code': '37069', 'lat': 45.35, 'lng': 10.84}
+        ] 
+
         shoppers = [
             {'id': 'S1', 'lat': 45.46, 'lng': 11.03, 'enabled': False}
         ]
@@ -26,6 +54,73 @@ class HaversineCoverageTest(unittest.TestCase):
         result = haversine_coverage(location, shoppers)
 
         self.assertEqual(result , [])
+
+    def test_coverage_shoppers(self):
+        location = [
+    	  {'id': 1000, 'zip_code': '37069', 'lat': 45.35, 'lng': 10.84},
+          {'id': 1001, 'zip_code': '37121', 'lat': 45.44, 'lng': 10.99}
+        ]
+
+        shoppers = [
+            {'id': 'S1', 'lat': 45.46, 'lng': 11.03, 'enabled': True},
+            {'id': 'S2', 'lat': 45.46, 'lng': 10.12, 'enabled': True},
+        ]
+
+        result = haversine_coverage(location, shoppers)
+
+        expected = [
+            {'shopper_id': 'S1', 'coverage': 50},
+            {'shopper_id': 'S2', 'coverage': 0},
+        ]
+        self.assertEqual(result , expected)
+
+    def test_full_lists(self):
+        locations = [
+    	  {'id': 1000, 'zip_code': '37069', 'lat': 45.35, 'lng': 10.84},
+    	  {'id': 1001, 'zip_code': '37121', 'lat': 45.44, 'lng': 10.99},
+    	  {'id': 1001, 'zip_code': '37129', 'lat': 45.44, 'lng': 11.00},
+          {'id': 1001, 'zip_code': '37133', 'lat': 45.43, 'lng': 11.02},            
+        ]
+
+        shoppers = [
+            {'id': 'S1', 'lat': 45.46, 'lng': 11.03, 'enabled': True},
+            {'id': 'S2', 'lat': 45.46, 'lng': 10.12, 'enabled': True},
+            {'id': 'S3', 'lat': 45.34, 'lng': 10.81, 'enabled': True},
+            {'id': 'S4', 'lat': 45.76, 'lng': 10.57, 'enabled': True},
+            {'id': 'S5', 'lat': 45.34, 'lng': 10.63, 'enabled': True},
+            {'id': 'S6', 'lat': 45.42, 'lng': 10.81, 'enabled': True},
+            {'id': 'S7', 'lat': 45.34, 'lng': 10.94, 'enabled': True},
+        ]
+
+        result = haversine_coverage(locations, shoppers)
+        expected = [
+            {'coverage': 75, 'shopper_id': 'S1'},
+            {'coverage': 25, 'shopper_id': 'S3'},
+            {'coverage': 25, 'shopper_id': 'S6'},
+            {'coverage': 25, 'shopper_id': 'S7'},
+            {'coverage': 0, 'shopper_id': 'S2'},
+            {'coverage': 0, 'shopper_id': 'S4'},
+            {'coverage': 0, 'shopper_id': 'S5'}
+        ]
+        self.assertEqual(result , expected)
+
+    def test_full_listsZZZZ(self):
+        locations = [
+    	  {'id': 1000, 'zip_code': '37069', 'lat': 45.35, 'lng': 10.84},
+    	  {'id': 1001, 'zip_code': '37121', 'lat': 45.44, 'lng': 10.99},
+    	  {'id': 1001, 'zip_code': '37129', 'lat': 45.44, 'lng': 11.00},
+          {'id': 1001, 'zip_code': '37133', 'lat': 45.43, 'lng': 11.02},            
+        ]
+
+        shoppers = [
+            {'id': 'S1', 'lat': 45.46, 'lng': 11.03, 'enabled': True},
+        ]
+
+        result = haversine_coverage(locations, shoppers)
+        expected = [
+            {'coverage': 75, 'shopper_id': 'S1'},
+        ]
+        self.assertEqual(result , expected)
 
 
 if __name__ == "__main__":
