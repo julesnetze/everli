9d8eb5d7ea870866d5ef065a3ca5108c164c7799
Jules Netze <julesnetze@gmail.com>
2020-12-17 13:12:03 +0100

    Create haversine coverage functionality

--- task_3/haversine_coverage.py
+++ task_3/haversine_coverage.py
@@ -1,5 +1,23 @@
 from haversine import haversine
 
+def haversine_coverage(locations, shoppers):
+    shoppers_list = []
+    
+    for shopper in shoppers:
+        if shopper.get('enabled') == False: continue
+        lat = shopper.get('lat')
+        lng = shopper.get('lng')
+        shopper_dict = {'shopper_id': shopper['id'], 'coverage': 0}
+
+        for location in locations:
+            if (haversine(location, (lat, lng)) < 10.0):
+                shopper_dict['coverage'] += 1
+
+        shoppers_list.append(shopper_dict)
+
+    return shoppers_list
+
+
 locations = [
     	  {'id': 1000, 'zip_code': '37069', 'lat': 45.35, 'lng': 10.84},
     	  {'id': 1001, 'zip_code': '37121', 'lat': 45.44, 'lng': 10.99},
@@ -17,3 +35,8 @@ shoppers = [
     {'id': 'S7', 'lat': 45.34, 'lng': 10.94, 'enabled': True},
 ]
 
+sorted = [
+  {'shopper_id': 'S3', 'coverage': 72},
+  {'shopper_id': 'S1', 'coverage': 43},
+  {'shopper_id': 'S6', 'coverage': 12},
+]
